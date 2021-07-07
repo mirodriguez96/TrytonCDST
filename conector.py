6e034c55bd5d47fa00d32676a1be88def2e0ba20
Lancheros <lancheros.cl@gmail.com>
2021-07-07 09:01:31 -0500

    prueba consulta 4

--- conector.py
+++ conector.py
@@ -37,7 +37,7 @@ class Terceros(ModelSQL, ModelView):
         Acterceros = Pool().get('conector.terceros')
         ultima_actualizacion = Acterceros.search([], order=[('id', 'DESC')], limit=1)
 
-        print(ultima_actualizacion.fecha)
+        print(ultima_actualizacion[0])
         """
         terceros_tecno = []
         try:
