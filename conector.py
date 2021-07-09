02d8b8cb6e089d283e7cf2b4fa428beb4ed4e1ef
Lancheros <lancheros.cl@gmail.com>
2021-07-09 09:30:29 -0500

    columnas4

--- conector.py
+++ conector.py
@@ -51,8 +51,8 @@ class Terceros(ModelSQL, ModelView):
         terceros_tecno = None
         try:
             with conexion.cursor() as cursor:
-                columnas = cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'TecnoCarnes' AND TABLE_NAME = 'dbo.TblTerceros'")
-                print(columnas)
+                columnas = cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'dbo.TblTerceros'")
+                print(columnas.fetchall())
                 query = cursor.execute("SELECT TOP(1) * FROM dbo.TblTerceros")
                 #print(query.fetchall()[0][0])
                 #terceros_tecno = json.loads(query.fetchall()[0][0])
