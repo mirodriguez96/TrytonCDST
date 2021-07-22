b5f4cb8892f883de38ca00f99a665b97f24f6dc5
Lancheros <lancheros.cl@gmail.com>
2021-07-22 10:52:53 -0500

    fix products

--- conector.py
+++ conector.py
@@ -194,11 +194,11 @@ class Terceros(ModelSQL, ModelView):
         Producto = Pool().get('product.product')
         Template_Product = Pool().get('product.template')
         Category = Pool().get('product.category')
-        ct, = Category.search([('id', '=', 12)])
+        #ct, = Category.search([('id', '=', 12)])
         to_prod = []
         
         for p in productos_tecno:
-            #ct, = Category.search([('id', '=', 1)])
+            ct, = Category.search([('id', '=', 12)])
             prod = Producto()
             temp = Template_Product()
             temp.name = p[col_pro.index('Producto')]
