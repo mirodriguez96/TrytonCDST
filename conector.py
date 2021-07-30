520b4813fb86d6231d054bef7167878269b82888
Lancheros <lancheros.cl@gmail.com>
2021-07-30 16:49:51 -0500

    fix cron

--- conector.py
+++ conector.py
@@ -180,7 +180,7 @@ class Terceros(ModelSQL, ModelView):
         print("---------------Run Productos---------------")
         Actualizacion = Pool().get('conector.terceros')
         ultima_actualizacion = Actualizacion.search([('actualizacion', '=','PRODUCTOS')], order=[('create_date', 'DESC')], limit=1)
-        print(ultima_actualizacion)
+        print(ultima_actualizacion.fecha)
         """
         productos_tecno = cls.get_data_db_tecno('TblProducto')
         col_pro = cls.get_columns_db_tecno('TblProducto')
