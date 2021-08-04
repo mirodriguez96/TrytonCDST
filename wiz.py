ee437660d0814bca48b8fa683d9c86cb43f1c244
Lancheros <lancheros.cl@gmail.com>
2021-08-04 14:57:37 -0500

    config asistente

--- wiz.py
+++ wiz.py
@@ -1,11 +1,17 @@
 from trytond.model import ModelView
 from trytond.wizard import Wizard, StateView, StateTransition, StateAction, Button
+import datetime
+from trytond.pool import Pool
+from trytond.transaction import Transaction
+from trytond.pyson import PYSONEncoder
+
 
 __all__ = [
     'ActualizarVentas',
     'CargarVentas',
     ]
 
+#Nota: el uso principal de los asistentes suele ser realizar acciones basadas en alguna entrada del usuario.
 
 class ActualizarVentas(Wizard):
     'ActualizarVentas'
@@ -18,18 +24,83 @@ class ActualizarVentas(Wizard):
     """
     __name__ = 'conector.terceros.actualizar_ventas'
 
-    print('Ejecutando Asistente')
-
-
+    #Estado inicial que le pedira al usuario una entrada
     start_state = 'parameters'
+    #vista donde se ingresa la entrada, que activa el siguiente estado
     parameters = StateView('conector.terceros.cargar_ventas.parameters',
         'conector.cargar_ventas_view_form', [
             Button('Cancel', 'end', 'tryton-cancel'),
             Button('Create', 'actualizar_venta', 'tryton-go-next', default=True)])
+    #Estado transitorio activado por la entrada del usuario
     actualizar_venta = StateTransition()
     #open_exemp = StateAction('conector.actualizar_venta')
+
+    @classmethod
+    def __setup__(cls):
+        super().__setup__()
+        cls._error_messages.update({
+                'invalid_model': 'This action should be started from a update',
+                })
+
+    def default_parameters(self, name):
+        #El context es un diccionario que contiene datos sobre el contexto (...) en el que se está ejecutando el código actual.
+        """
+        active_model: es el modelo que se muestra actualmente al usuario. En nuestro caso,conector.terceros
+        active_ids: es la lista de los ID de los registros que se seleccionaron cuando se desencadenó la acción
+        active_id: es el primero de esos registros (o el único si solo se seleccionó un registro).
+        """
+        if Transaction().context.get('active_model', '') != 'conector.terceros':
+            #generamos un mensaje de error
+            self.raise_user_error('invalid_model')
+        return {
+            'date': datetime.date.today(),
+            'actualizacion': Transaction().context.get('active_id'),
+            }
+
+    """
+    def do_open_exemplaries(self, action):
+        #Aquí configuramos la clave pyson_domain para forzar un dominio / restricción en la pestaña,
+        #lo que hará que muestre solo los identificadores que coinciden con los elementos que acabamos de crear.
+        action['pyson_domain'] = PYSONEncoder().encode([
+                ('id', 'in', [x.id for x in self.parameters.exemplaries])])
+        return action, {}
+    """
+
+
 
 
 class CargarVentas(ModelView):
     'CargarVentas'
     __name__ = 'conector.terceros.cargar_ventas.parameters'
+
+    @classmethod
+    def __setup__(cls):
+        super().__setup__()
+        cls._error_messages.update({
+                'invalid_date' : 'No puede seguir',
+                })
+
+    def transition_actualizar_venta(self):
+        if (self.parameters.date and self.parameters.date > datetime.date.today()):
+            self.raise_user_error('invalid_date')
+        #Si no...
+        print(self.parameters.fecha)
+        """
+        Exemplary = Pool().get('library.book.exemplary')
+        to_create = []
+        while len(to_create) < self.parameters.number_of_exemplaries:
+            exemplary = Exemplary()
+            exemplary.book = self.parameters.book
+            exemplary.acquisition_date = self.parameters.acquisition_date
+            exemplary.acquisition_price = self.parameters.acquisition_price
+            exemplary.identifier = self.parameters.identifier_start + str(
+                len(to_create) + 1)
+            to_create.append(exemplary)
+        Exemplary.save(to_create)
+        self.parameters.exemplaries = to_create
+        return 'open_exemplaries'
+        """
+
+
+if self.parameters._default_values:
+    return self.parameters._default_values
