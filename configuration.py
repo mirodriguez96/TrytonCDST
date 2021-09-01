342e7120efa0515a3e0599dfddb1d36b0d6ce2ba
Lancheros <lancheros.cl@gmail.com>
2021-09-01 10:12:51 -0500

    Fix Config Conector

--- configuration.py
+++ configuration.py
@@ -26,7 +26,7 @@ class Configuration(ModelSQL, ModelView):
     @ModelView.button
     def test_conexion(cls, records):
         print('TEST CONEXION:')
-        print((cls.records))
+        print(cls.db[0])
         """
         try:
             conexion = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+str(cls.server)+';DATABASE='+str(cls.db)+';UID='+str(cls.user)+';PWD='+str(cls.password))
