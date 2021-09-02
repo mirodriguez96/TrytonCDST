6b2e2839aaab31ff68fed44fabf0394735e9565d
Lancheros <lancheros.cl@gmail.com>
2021-09-02 16:22:45 -0500

    full test ubuntu 20.04

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-"""
-El archivo setup.py describe cómo debe instalarse su módulo.
-"""
-
-#!/usr/bin/env python3
-# This file is part of Tryton.  The COPYRIGHT file at the top level of
-# this repository contains the full copyright notices and license terms.
-
-import io
-import os
-import re
-from configparser import ConfigParser
 from setuptools import setup
+import re
+import os
+import io
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser
+
+MODULE = 'conector'
+PREFIX = 'trytoncdst'
+MODULE2PREFIX = {}
 
 
 def read(fname):
@@ -28,9 +27,8 @@ def get_require_version(name):
         major_version, minor_version + 1)
     return require
 
-
 config = ConfigParser()
-config.read_file(open('tryton.cfg'))
+config.readfp(open('tryton.cfg'))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
@@ -39,7 +37,7 @@ version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-name = 'trytond_conector'
+name = '%s_%s' % (PREFIX, MODULE)
 
 download_url = 'http://downloads.tryton.org/%s.%s/' % (
     major_version, minor_version)
@@ -49,7 +47,7 @@ if minor_version % 2:
         'hg+http://hg.tryton.org/modules/%s#egg=%s-%s' % (
             name[8:], name, version))
 
-requires = ['python-sql >= 0.9']
+requires = ['python-sql >= 0.4']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
@@ -58,33 +56,61 @@ requires.append(get_require_version('trytond'))
 tests_require = [get_require_version('proteus')]
 dependency_links = []
 if minor_version % 2:
+    # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')
 
 setup(name=name,
     version=version,
-    description='Modulo conector de CDST',
-    author='Casa de Software Tecno SAS',
-    author_email='issue_tracker@tryton.org',
-    url='http://www.tryton.org/',
-    download_url=download_url,
-    keywords='tryton conector',
-    package_dir={'trytond.modules.conector': '.'},
+    description='Tryton module cdst conector',
+    long_description=read('README'),
+    author='Cristhian Lancheros',
+    author_email='clancheros@cdst.com',
+    url='http://www.cdst.com/',
+    download_url="https://github.com/lancheros/%s" % name,
+    keywords='tryton %s' % MODULE,
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.conector',
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
         ],
     package_data={
-        'trytond.modules.conector': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'icons/*.svg',
-            'tests/*.rst']),
+        'trytond.modules.%s' % MODULE: (info.get('xml', [])
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
+                '*.fods']),
         },
-    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Framework :: Tryton',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Legal Industry',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Natural Language :: Spanish',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Office/Business',
+        'Topic :: Office/Business :: Financial :: Accounting',
+        ],
     license='GPL-3',
-    python_requires='>=3.4',
-    install_requires=requires,
+    install_requires=None,
     dependency_links=dependency_links,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    conector = trytond.modules.conector
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
+    test_suite='tests',
+    test_loader='trytond.test_loader:Loader',
+    tests_require=tests_require,
+    use_2to3=True,
+    convert_2to3_doctests=[
+        'tests/scenario.rst',
+        ],
     )
