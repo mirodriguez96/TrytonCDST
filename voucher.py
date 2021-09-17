8f58d143513243a3cbf96e0ae64b7920814db316
Lancheros <lancheros.cl@gmail.com>
2021-09-17 14:34:06 -0500

    fix minor update_paymode

--- voucher.py
+++ voucher.py
@@ -92,11 +92,10 @@ class Voucher(ModelSQL, ModelView):
             idt = str(fp[columns_fp.index('IdFormaPago')])
             paym = PayMode.search([('id_tecno', '=', idt)])
             if paym:
-                print(paym)
                 for pm in paym:
                     print(pm)
                     pm.name = fp[columns_fp.index('FormaPago')]
-                    paym.save()
+                    PayMode.save(paym)
             else:
                 journal, = Journal.search([('code', '=', 'REV')])
                 paym = PayMode()
