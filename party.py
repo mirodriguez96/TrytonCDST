787e4b06c111cb618af4ae4f921dabd2a5d4b086
Cristhian <lancheros.cl@gmailcom>
2021-09-27 11:37:01 -0500

    test parties duplicate

--- party.py
+++ party.py
@@ -399,7 +399,6 @@ class Party(ModelSQL, ModelView):
                 fecha = (ultima_actualizacion[0].create_date - datetime.timedelta(hours=5))
         else:
             fecha = datetime.date(1,1,1)
-            cls.create_actualizacion(True)
         fecha = fecha.strftime('%Y-%d-%m %H:%M:%S')
         data = cls.get_data_where_tecno('TblTerceros', fecha)
         return data
