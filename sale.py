056d5166d2faa2f9ed7f0468570f93a03ba3533f
Cristhian <lancheros.cl@gmailcom>
2021-09-30 09:44:55 -0500

    sale confirmed

--- sale.py
+++ sale.py
@@ -127,10 +127,10 @@ class Sale(metaclass=PoolMeta):
                         else:
                             raise UserError("Error, no existe el producto con la siguiente id: ", str(lin[col_line.index('IdProducto')]))
                     #Procesamos la venta para generar la factura y procedemos a rellenar los campos de la factura
-                    venta.state = 'confirmed'
-                    Sale.process([venta])
+                    venta.confirm()
+                    venta.proceed()
                     if venta.state == 'confirmed':
-                        Sale.process([venta])
+                        venta.process()
                     invoice, = venta.invoices
                     venta.save()
                     invoice.operation_type = 10
