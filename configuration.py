79ea1a0dc0562f2291fb557a63c2475d8bcf59b2
lancheros <lancheros.cl@gmail.com>
2022-08-02 17:57:56 -0500

    fix minor

--- configuration.py
+++ configuration.py
@@ -102,7 +102,7 @@ class Configuration(ModelSQL, ModelView):
     @classmethod
     def get_tblterceros(cls, fecha):
         fecha = fecha.strftime('%Y-%m-%d %H:%M:%S')
-        query = f"SET DATEFORMAT ymd SELECT * FROM dbo.TblTerceros WHERE fecha_creacion >= CAST('"+fecha+"' AS datetime) OR Ultimo_Cambio_Registro >= CAST('"+fecha+"' AS datetime)"
+        query = "SET DATEFORMAT ymd SELECT * FROM dbo.TblTerceros WHERE fecha_creacion >= CAST('"+fecha+"' AS datetime) OR Ultimo_Cambio_Registro >= CAST('"+fecha+"' AS datetime)"
         data = cls.get_data(query)
         return data
 
@@ -115,7 +115,7 @@ class Configuration(ModelSQL, ModelView):
 
     @classmethod
     def get_tercerosdir_nit(cls, nit):
-        query = f"SELECT * FROM dbo.Terceros_Dir WHERE nit = {nit}"
+        query = "SELECT * FROM dbo.Terceros_Dir WHERE nit = "+nit
         data = cls.get_data(query)
         return data
 
