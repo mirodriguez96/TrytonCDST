314270a4a2572875f57f5008e1afbf8b89a11a30
lancheros <lancheros.cl@gmail.com>
2022-08-12 17:16:35 -0500

    fix account

--- configuration.py
+++ configuration.py
@@ -469,7 +469,6 @@ class Configuration(ModelSQL, ModelView):
             account.name = name
             account.reconcile = reconcile
             account.party_required = party_required
-            account.type = None
             if type:
                 type = Type.search([('sequence', '=', type)])
                 if not type:
@@ -483,14 +482,10 @@ class Configuration(ModelSQL, ModelView):
                     not_account.append(code_parent)
                     continue
                 account.parent = parent_account[0]
-            account.save()
-            # if account.id:
-            #     account.save()
-            # else:
-            #     to_save.append(account)
+            to_save.append(account)
         if not_account:
             raise UserError('Importación de archivo: ', f'Error: Faltan las cuentas padres {not_account}')
-        #Account.save(to_save)
+        Account.save(to_save)
 
     
     @classmethod
