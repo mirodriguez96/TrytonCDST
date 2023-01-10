a792b6a7ba2deb94b63da511ab5d6ab8f315fb1f
lancheros <lancheros.cl@gmail.com>
2023-01-10 12:08:32 -0500

    fix bug frg

--- wiz.py
+++ wiz.py
@@ -40,17 +40,29 @@ class FixBugsConector(Wizard):
         #     Sale.post_invoices(sale)
 
         cursor = Transaction().connection.cursor()
-        sale_table = Table('sale_sale')
-        Sale = pool.get('sale.sale')
+        voucher_table = Table('account_voucher')
+        Voucher = pool.get('account.voucher')
         start_date = datetime.datetime(2022,12,22)
         end_date = datetime.datetime(2023,1,4)
-        cursor.execute(*sale_table.select(sale_table.id,
-            where= (sale_table.create_date >= start_date) & (sale_table.create_date < end_date)
+        # Reimporta comprobantes de ingreso
+        cursor.execute(*voucher_table.select(voucher_table.id,
+            where= (voucher_table.create_date >= start_date) & (voucher_table.create_date < end_date) & (voucher_table.id_tecno != None)
             ))
-        for sale_id in cursor.fetchall():
-            sale = Sale(sale_id[0])
-            print(sale)
-            Sale.delete_imported_sales([sale])
+        for voucher_id in cursor.fetchall():
+            voucher = Voucher(voucher_id[0])
+            print(voucher)
+            Voucher.delete_imported_vouchers([voucher])
+            Transaction().connection.commit()
+        # Reimporta multi-ingresos
+        multirevenue_table = Table('account_multirevenue')
+        Multirevenue = pool.get('account.multirevenue')
+        cursor.execute(*multirevenue_table.select(multirevenue_table.id,
+            where= (multirevenue_table.create_date >= start_date) & (multirevenue_table.create_date < end_date) & (multirevenue_table.id_tecno != None)
+            ))
+        for multirevenue_id in cursor.fetchall():
+            multirevenue = Multirevenue(multirevenue_id[0])
+            print(multirevenue)
+            Multirevenue.mark_rimport([multirevenue])
             Transaction().connection.commit()
 
         return 'end'
