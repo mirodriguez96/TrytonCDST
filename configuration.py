fea5edc2dc4b1bfa864aca20f60983f07f26ccdb
genes15 <andresgenes49@gmail.com>
2023-03-17 08:53:55 -0500

    fix sw in productions

--- configuration.py
+++ configuration.py
@@ -152,7 +152,10 @@ class Configuration(ModelSQL, ModelView):
         config, = Config.search([], order=[('id', 'DESC')], limit=1)
         fecha = config.date.strftime('%Y-%m-%d %H:%M:%S')
         #query = "SELECT * FROM dbo.Documentos WHERE tipo = 110 AND Numero_documento=127016" #TEST
-        query = "SET DATEFORMAT ymd SELECT TOP(50) * FROM dbo.Documentos WHERE fecha_hora >= CAST('"+fecha+"' AS datetime) AND sw = "+sw+" AND tipo = "+tipo+" AND exportado != 'T' AND exportado != 'E' AND exportado != 'X' "
+        if not sw:
+            query = "SET DATEFORMAT ymd SELECT TOP(50) * FROM dbo.Documentos WHERE fecha_hora >= CAST('"+fecha+"' AS datetime) AND tipo = "+tipo+" AND exportado != 'T' AND exportado != 'E' AND exportado != 'X' "
+        else:
+            query = "SET DATEFORMAT ymd SELECT TOP(50) * FROM dbo.Documentos WHERE fecha_hora >= CAST('"+fecha+"' AS datetime) AND sw = "+sw+" AND tipo = "+tipo+" AND exportado != 'T' AND exportado != 'E' AND exportado != 'X' "
         if config.end_date:
             end_date = config.end_date.strftime('%Y-%m-%d %H:%M:%S')
             query += "AND fecha_hora < CAST('"+end_date+"' AS datetime) "
