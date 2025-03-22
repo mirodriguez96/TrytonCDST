47d89526cbc1424ef18bfcb5f4d7b6050091d5b3
Miguel Rodriguez Orejuela <mirodriguezor@unal.edu.co>
2025-03-22 04:21:42 +0000

    Resolve "Modularizacion staff_liquidation"

---
+++ staff_liquidation_cdst/__init__.py
@@ -0,0 +1,28 @@
+# This file is part of trytond-staff-liquidation-cdst.
+# Licensed under the GNU General Public License v3 or later (GPLv3+).
+# The COPYRIGHT file at the top level of this repository contains the
+# full copyright notices and license terms.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from trytond.pool import Pool
+
+__all__ = ['register']
+
+from . import liquidation
+
+
+def register():
+    Pool.register(
+        liquidation.Liquidation,
+        liquidation.LiquidationLine,
+        liquidation.AnalyticAccountEntry,
+        module='staff_liquidation_cdst', type_='model')
+
+    Pool.register(
+        liquidation.MoveProvisionBonusService,
+        liquidation.LiquidationGroup,
+        module='staff_liquidation_cdst', type_='wizard')
+
+    Pool.register(
+        liquidation.LiquidationReport,
+        module='staff_liquidation_cdst', type_='report')
